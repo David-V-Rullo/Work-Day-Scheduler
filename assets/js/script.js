5cba69f6fe723f35800dbe24e0af5fd5874039cf
David-V-Rullo <d.v.rullo@gmail.com>
2021-05-15 14:57:13 -0400

    Almost working. Just need to get teh save button to write to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,42 @@
 //Get Current times and store for later
 var currentDate = moment().format('dddd MMM Do, YYYY')
 var currentTime = moment().format('hh:mm a ')
+var currentHour = moment().hour()
+console.log(currentHour)
 //Find the time and date elements
 var timeEl = $("#currentTime")
 var dateEL = $("#currentDay")
-console.log(currentDate)
-console.log(currentTime)
-//Set current time and date to respective elements
-dateEL.text(currentDate)
-timeEl.text(currentTime)
+var timeBlockArr = $(".row.time-block").toArray()
 
+timeEl.html(currentTime)
+dateEL.html(currentDate)
 //Create a function to render the hour blocks based on their place in time; use past present and future .addClass
-function renderTimes(){
-    for (i=1; i === 11; i++ ){
-        if () {
 
+function renderTimes(timeBlockArr){
+    $(".row.time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id"))
+        if (blockHour === currentHour){
+            $(this).removeClass("past")
+            $(this).addClass("present")
+            $(this).removeClass("future")
         }
+        else if (blockHour > currentHour){
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+            $(this).addClass("future")
+        }
+        else {
+            $(this).addClass("past")
+            $(this).removeClass("present")
+            $(this).removeClass("future")
+        }
+        //Add and remove class formats
+        // $(this).addClass("past")
+        // $(this).addClass("present")
+        // $(this).addClass("future")
+    } )
 }
-$("#1").addClass("present")
-$("#2").addClass("future")
+renderTimes()
+// $("#1").addClass("present")
+// $("#2").addClass("future")
 
